7ec46d38b273429d3dd5b3d84c73f8f7b1e7d89b
RF Nelson <rnelson@alumni.upenn.edu>
2015-03-17 18:02:31 -0400

    add edit button

--- app/assets/javascripts/views/listings/listing_show.js
+++ app/assets/javascripts/views/listings/listing_show.js
@@ -15,6 +15,39 @@ Petadopter.Views.ListingShow = Backbone.View.extend({
     })
   },
 
+  // this function creates an "Edit Listing" button on the dialog box
+  // if the current user originally created the listing.
+  // Below I started to replace the existing dialog content with the
+  // edit form but I think it would be easier to remove the dialog box
+  // and replace it with a new dialog box made for editing comments
+  dialogButtons: function () {
+    var that = this;
+
+    if (Petadopter.currentUserId === this.modelId) {
+      return {"Edit Listing": function () {
+        var $boxContent = $("div#dialog-" + that.modelId)
+        $boxContent.html('')
+        var view = JST['listings/form']({ listing: that.model, userId: Petadopter.currentUserId })
+        $boxContent.html(view)
+          // var attrs = $("#newListing").serializeJSON()
+          //
+          // that.model.set(attrs)
+          // that.model.save({}, {
+          //   success: function () {
+          //     that.collection.add(that.model, {merge: true})
+          //     $(dialog).dialog('close')
+          //   },
+          //   error: function () {
+          //     $("form#newListing").prepend("<h4 style='color:red'>Please make sure your new adoption listing has a Pet Name, Location, and Description.</h4></style>")
+          //   }
+          // })
+        }
+      }
+    } else {
+      return null
+    }
+  },
+
   render: function () {
     // $("body").scroll(function(e){ e.preventDefault()});
     // $('body').addClass('stop-scrolling')
@@ -32,6 +65,7 @@ Petadopter.Views.ListingShow = Backbone.View.extend({
         effect: "blind",
         duration: 700
       },
+      buttons: that.dialogButtons(),
       close: function () {
         $("div#dialog-" + that.modelId).remove()
       },
